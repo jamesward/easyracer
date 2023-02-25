e8ab3bb10520c2cb4e1bf572966d5ba1ce2f8e63
Jack Leow <jackgene@gmail.com>
2023-02-25 08:11:33 -0800

    Set platform to .macOS(.v10_14), which should be Linux compatible.

--- swift-dispatch/Package.swift
+++ swift-dispatch/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "EasyRacer",
+    platforms: [
+        .macOS(.v10_14)
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
