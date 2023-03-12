b4c8a477cc12e48e67409d2b256a144bb6aa8c11
Jack Leow <jackgene@gmail.com>
2023-03-12 14:27:57 -0700

    Implemented Scenario 6.

--- elm-worker/src/EasyRacer/Scenario6.elm
+++ elm-worker/src/EasyRacer/Scenario6.elm
@@ -1,6 +1,8 @@
 port module EasyRacer.Scenario6 exposing (main)
 
+import Http
 import Platform exposing (Program)
+import Set exposing (Set)
 
 
 type alias ScenarioResult =
@@ -17,21 +19,86 @@ type alias Flags =
 
 
 type alias Model =
-    ()
+    { inflightTrackers : Set String
+    }
 
 
-type alias Msg =
-    ()
+type Msg
+    = HttpResponse String (Result Http.Error String)
+
+
+scenarioPath : String
+scenarioPath =
+    "/6"
 
 
 init : Flags -> ( Model, Cmd Msg )
-init _ =
-    ( (), sendResult_ { isError = True, value = "not implemented yet" } )
+init baseUrl =
+    let
+        requestTrackers =
+            [ "first", "second", "third" ]
+
+        httpRequest tracker =
+            { method = "GET"
+            , headers = []
+            , url = baseUrl ++ scenarioPath
+            , body = Http.emptyBody
+            , expect = Http.expectString (HttpResponse tracker)
+            , timeout = Nothing
+            , tracker = Just tracker
+            }
+    in
+    ( { inflightTrackers = Set.fromList requestTrackers
+      }
+    , requestTrackers
+        |> List.map (Http.request << httpRequest)
+        |> Cmd.batch
+    )
+
+
+sendResult : Result String String -> Cmd Msg
+sendResult result =
+    sendResult_ <|
+        case result of
+            Ok value ->
+                { isError = False, value = value }
+
+            Err error ->
+                { isError = True, value = error }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        HttpResponse tracker httpResult ->
+            let
+                remainingTrackers : Set String
+                remainingTrackers =
+                    model.inflightTrackers |> Set.remove tracker
+            in
+            ( { model | inflightTrackers = remainingTrackers }
+            , case httpResult of
+                Ok bodyText ->
+                    let
+                        cancellations : List (Cmd Msg)
+                        cancellations =
+                            remainingTrackers
+                                |> Set.toList
+                                |> List.map Http.cancel
+
+                        returnResult : Cmd Msg
+                        returnResult =
+                            Ok bodyText |> sendResult
+                    in
+                    returnResult :: cancellations |> Cmd.batch
+
+                Err _ ->
+                    if not <| Set.isEmpty remainingTrackers then
+                        Cmd.none
+
+                    else
+                        Err "all requests completed without a single success response" |> sendResult
+            )
 
 
 main : Program Flags Model Msg
