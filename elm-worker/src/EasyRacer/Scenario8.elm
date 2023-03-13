25905b9eb62ead1f84dcf1c7f8213b3641e84780
Jack Leow <jackgene@gmail.com>
2023-03-12 17:24:50 -0700

    Scenario 8. 🤞🤞

--- elm-worker/src/EasyRacer/Scenario8.elm
+++ elm-worker/src/EasyRacer/Scenario8.elm
@@ -1,6 +1,9 @@
 port module EasyRacer.Scenario8 exposing (main)
 
+import Http
 import Platform exposing (Program)
+import Set exposing (Set)
+import Task
 
 
 type alias ScenarioResult =
@@ -17,21 +20,108 @@ type alias Flags =
 
 
 type alias Model =
-    ()
+    { baseUrl : String
+    , result : Maybe String
+    , inflightTrackers : Set String
+    }
 
 
-type alias Msg =
-    ()
+type Msg
+    = OpenHttpResponse String (Result Http.Error String)
+    | UseHttpResponse String String (Result Http.Error String)
+    | CloseHttpResponse String
+
+
+scenarioPath : String
+scenarioPath =
+    "/8"
 
 
 init : Flags -> ( Model, Cmd Msg )
-init _ =
-    ( (), sendResult_ { isError = True, value = "not implemented yet" } )
+init baseUrl =
+    let
+        requestTrackers =
+            [ "first", "second" ]
+
+        httpRequest tracker =
+            { method = "GET"
+            , headers = []
+            , url = baseUrl ++ scenarioPath ++ "?open"
+            , body = Http.emptyBody
+            , expect = Http.expectString (OpenHttpResponse tracker)
+            , timeout = Nothing
+            , tracker = Just tracker
+            }
+    in
+    ( { baseUrl = baseUrl
+      , result = Nothing
+      , inflightTrackers = Set.fromList requestTrackers
+      }
+    , requestTrackers
+        |> List.map (Http.request << httpRequest)
+        |> Cmd.batch
+    )
+
+
+sendResult : Result String String -> Cmd Msg
+sendResult result =
+    sendResult_ <|
+        case result of
+            Ok value ->
+                { isError = False, value = value }
+
+            Err error ->
+                { isError = True, value = error }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        OpenHttpResponse tracker httpResult ->
+            ( model
+            , case httpResult of
+                Ok id ->
+                    Http.request
+                        { method = "GET"
+                        , headers = []
+                        , url = model.baseUrl ++ scenarioPath ++ "?use=" ++ id
+                        , body = Http.emptyBody
+                        , expect = Http.expectString (UseHttpResponse tracker id)
+                        , timeout = Nothing
+                        , tracker = Just tracker
+                        }
+
+                Err _ ->
+                    Task.succeed tracker |> Task.perform CloseHttpResponse
+            )
+
+        UseHttpResponse tracker id httpResult ->
+            ( { model | result = Result.toMaybe httpResult }
+            , Http.request
+                { method = "GET"
+                , headers = []
+                , url = model.baseUrl ++ scenarioPath ++ "?close=" ++ id
+                , body = Http.emptyBody
+                , expect = Http.expectWhatever (\_ -> CloseHttpResponse tracker)
+                , timeout = Nothing
+                , tracker = Just tracker
+                }
+            )
+
+        CloseHttpResponse tracker ->
+            let
+                remainingTrackers : Set String
+                remainingTrackers =
+                    model.inflightTrackers |> Set.remove tracker
+            in
+            ( { model | inflightTrackers = remainingTrackers }
+            , if not <| Set.isEmpty remainingTrackers then
+                Cmd.none
+
+              else
+                Result.fromMaybe "all requests completed without a single success response" model.result
+                    |> sendResult
+            )
 
 
 main : Program Flags Model Msg
