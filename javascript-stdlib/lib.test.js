239790b598cebb9a52fdf086c4634c8c13c4d287
James Ward <james@jamesward.com>
2023-08-29 13:57:43 -0600

    debug

--- javascript-stdlib/lib.test.js
+++ javascript-stdlib/lib.test.js
@@ -18,6 +18,7 @@ describe("all work", () => {
         container = await new GenericContainer("ghcr.io/jamesward/easyracer")
             .withExposedPorts(8080)
             .withWaitStrategy(Wait.forHttp("/", 8080))
+            .withCommand(["--debug"])
             .start()
     }, 15000);
 
