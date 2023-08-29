8fb88f621fa9c78d67a5dc5d347d63456310ff54
James Ward <james@jamesward.com>
2023-08-29 13:50:01 -0600

    try even more time

--- javascript-stdlib/lib.test.js
+++ javascript-stdlib/lib.test.js
@@ -36,5 +36,5 @@ describe("all work", () => {
         expect(await scenario7(httpPort)).toBe("right")
         expect(await scenario8(httpPort)).toBe("right")
         expect(await scenario9(httpPort)).toBe("right")
-    }, 60000)
+    }, 600000)
 })
