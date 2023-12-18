e25768f00270cea01f02fd22a4633bb77eed8993
James Ward <james@jamesward.com>
2023-12-18 00:18:51 -0700

    bump and fix testcontainer kotest

--- kotlin-arrow/src/test/kotlin/MainSpec.kt
+++ kotlin-arrow/src/test/kotlin/MainSpec.kt
@@ -1,18 +1,22 @@
 import io.kotest.core.extensions.install
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.extensions.testcontainers.TestContainerExtension
+import io.kotest.extensions.testcontainers.ContainerExtension
 import io.kotest.matchers.collections.shouldContainOnly
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.images.PullPolicy
 
 class MainSpec : FunSpec() {
     init {
-        val container = install(TestContainerExtension("ghcr.io/jamesward/easyracer")) {
-            withImagePullPolicy(PullPolicy.alwaysPull())
-            withExposedPorts(8080)
-        }
+        val container = GenericContainer("ghcr.io/jamesward/easyracer")
+            .withExposedPorts(8080)
+            .withImagePullPolicy(PullPolicy.alwaysPull())
+            .waitingFor(Wait.forHttp("/"))
+
+        val server = install(ContainerExtension(container))
 
         test("should work") {
-            fun url(i: Int) = "http://localhost:${container.firstMappedPort}/$i"
+            fun url(i: Int) = "http://localhost:${server.firstMappedPort}/$i"
             results(::url).shouldContainOnly("right")
         }
     }
