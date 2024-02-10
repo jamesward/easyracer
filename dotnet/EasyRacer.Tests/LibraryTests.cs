ff6fc03792233617e5155ddc4c3f9a32dda0d2e4
delormej <delormej@gmail.com>
2024-02-10 15:29:59 -0500

    Unit test working with test containers

--- dotnet/EasyRacer.Tests/LibraryTests.cs
+++ dotnet/EasyRacer.Tests/LibraryTests.cs
@@ -1,13 +1,44 @@
+namespace EasyRacer.Tests;
 
-namespace EasyRacer.Tests
+public class LibraryTests
 {
-    public class LibraryTests
+    const string ContainerImage = "ghcr.io/jamesward/easyracer";
+    const int Port = 8080;
+
+    [Fact]
+    public async void TestScenario1()
     {
-        [Fact]
-        public async void TestScenario1()
-        {
-            var lib = new Library(); 
-            await lib.Scenario1(8080);
-        }
+        // Create a new instance of a container.
+        var container = new ContainerBuilder()
+            .WithImage(ContainerImage)
+            // Bind port 8080 of the container to a random port on the host.
+            .WithPortBinding(Port, true)
+            // Wait until the HTTP endpoint of the container is available.
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(Port)))
+            // Build the container configuration.
+            .Build();
+
+        // Start the container.
+        await container.StartAsync().ConfigureAwait(false);
+
+        // Get the port of the container.
+        var port = container.GetMappedPublicPort(Port);
+
+
+        // Create a new instance of HttpClient to send HTTP requests.
+        var httpClient = new HttpClient();
+
+        var lib = new Library(httpClient); 
+
+        Assert.Equal("right", await lib.Scenario1(port));
+        Assert.Equal("right", await lib.Scenario2(port));
+        Assert.Equal("right", await lib.Scenario3(port));
+        Assert.Equal("right", await lib.Scenario4(port));
+        Assert.Equal("right", await lib.Scenario5(port));
+        Assert.Equal("right", await lib.Scenario6(port));
+        Assert.Equal("right", await lib.Scenario7(port));
+        Assert.Equal("right", await lib.Scenario8(port));
+        Assert.Equal("right", await lib.Scenario9(port));
+        Assert.Equal("right", await lib.Scenario10(port));
     }
 }
