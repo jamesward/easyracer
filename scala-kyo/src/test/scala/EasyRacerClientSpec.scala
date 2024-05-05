f50858084da9a48e6341fe092dc50f502c89a6fc
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2024-05-04 20:56:11 -0600

    Convert Kyo runner to zio-test

--- scala-kyo/src/test/scala/EasyRacerClientSpec.scala
+++ scala-kyo/src/test/scala/EasyRacerClientSpec.scala
@@ -1,39 +1,37 @@
 import com.dimafeng.testcontainers.GenericContainer
 import kyo.*
-import org.scalatest.BeforeAndAfterAll
-import org.scalatest.flatspec.AsyncFlatSpec
-import org.scalatest.matchers.should.Matchers
+import kyo.test.*
+import zio.ZIO
+import zio.test.*
 import org.testcontainers.containers.wait.strategy.Wait
+
 import org.testcontainers.images.PullPolicy
 import sttp.client3.UriContext
 
 import scala.compiletime.uninitialized
 import scala.concurrent.{ExecutionContext, Future}
 
-class EasyRacerClientSpec extends AsyncFlatSpec with Matchers with BeforeAndAfterAll:
-
-  implicit override def executionContext: ExecutionContext = ExecutionContext.global
+object EasyRacerClientSpec extends KyoSpecDefault:
 
   var container: GenericContainer = uninitialized
   var port: Int = 0
-
-  override protected def beforeAll(): Unit =
-    super.beforeAll()
-    container = GenericContainer(
-      "ghcr.io/jamesward/easyracer",
-      Seq(8080),
-      waitStrategy = Wait.forHttp("/"),
-      imagePullPolicy = PullPolicy.alwaysPull()
-    )
-    container.start()
-    port = container.container.getFirstMappedPort
-
-  override protected def afterAll(): Unit =
-    container.stop()
-    super.afterAll()
-
   def scenarioUrl(scenario: Int) = uri"http://localhost:$port/$scenario"
 
-  EasyRacerClient.scenarios.zipWithIndex.foreach: (fn, number) =>
-    s"scenario ${number + 1}" should "work" in:
-      IOs.run(KyoApp.runFiber(fn(scenarioUrl)).toFuture).map(_.get).map(_ shouldBe "right")
+  def spec =
+    suite("KyoEasyRacer") {
+      EasyRacerClient.scenarios.zipWithIndex.map: (fn, number) =>
+        test(s"scenario ${number + 1}") {
+          fn(scenarioUrl).map(result => assertTrue(result == "right"))
+        }
+    } @@ TestAspect.sequential @@
+      TestAspect.before(ZIO.attempt{
+        container = GenericContainer(
+          "ghcr.io/jamesward/easyracer",
+          Seq(8080),
+          waitStrategy = Wait.forHttp("/"),
+          imagePullPolicy = PullPolicy.alwaysPull()
+        )
+        container.start()
+        port = container.container.getFirstMappedPort
+      }) @@
+      TestAspect.after(ZIO.attempt(container.stop()))
