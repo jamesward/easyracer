7a676bdd4dc2a99011082f2e44f52cd26e8c177f
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2024-05-09 09:09:37 -0600

    Revert "Convert Kyo runner to zio-test"
    This reverts commit 4f280640ada4c69d4c52ba25fbac25bec87b26c4.

--- scala-kyo/build.sbt
+++ scala-kyo/build.sbt
@@ -5,8 +5,7 @@ libraryDependencies ++= Seq(
   "io.getkyo" %% "kyo-direct" % "0.9.3",
   "io.getkyo" %% "kyo-sttp" % "0.9.3",
   "org.slf4j" % "slf4j-simple" % "2.0.13",
-  "io.getkyo" %% "kyo-test" % "0.9.3" % Test,
-  "dev.zio" %% "zio-test-sbt" % "2.0.22" % Test,
+  "org.scalatest" %% "scalatest" % "3.2.18" % Test,
   "com.dimafeng" %% "testcontainers-scala-core" % "0.41.3" % Test
 )
 
--- scala-kyo/src/test/scala/EasyRacerClientSpec.scala
+++ scala-kyo/src/test/scala/EasyRacerClientSpec.scala
@@ -1,37 +1,39 @@
 import com.dimafeng.testcontainers.GenericContainer
 import kyo.*
-import kyo.test.*
-import zio.ZIO
-import zio.test.*
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
 import org.testcontainers.containers.wait.strategy.Wait
-
 import org.testcontainers.images.PullPolicy
 import sttp.client3.UriContext
 
 import scala.compiletime.uninitialized
 import scala.concurrent.{ExecutionContext, Future}
 
-object EasyRacerClientSpec extends KyoSpecDefault:
+class EasyRacerClientSpec extends AsyncFlatSpec with Matchers with BeforeAndAfterAll:
+
+  implicit override def executionContext: ExecutionContext = ExecutionContext.global
 
   var container: GenericContainer = uninitialized
   var port: Int = 0
+
+  override protected def beforeAll(): Unit =
+    super.beforeAll()
+    container = GenericContainer(
+      "ghcr.io/jamesward/easyracer",
+      Seq(8080),
+      waitStrategy = Wait.forHttp("/"),
+      imagePullPolicy = PullPolicy.alwaysPull()
+    )
+    container.start()
+    port = container.container.getFirstMappedPort
+
+  override protected def afterAll(): Unit =
+    container.stop()
+    super.afterAll()
+
   def scenarioUrl(scenario: Int) = uri"http://localhost:$port/$scenario"
 
-  def spec =
-    suite("KyoEasyRacer") {
-      EasyRacerClient.scenarios.zipWithIndex.map: (fn, number) =>
-        test(s"scenario ${number + 1}") {
-          fn(scenarioUrl).map(result => assertTrue(result == "right"))
-        }
-    } @@ TestAspect.sequential @@
-      TestAspect.before(ZIO.attempt{
-        container = GenericContainer(
-          "ghcr.io/jamesward/easyracer",
-          Seq(8080),
-          waitStrategy = Wait.forHttp("/"),
-          imagePullPolicy = PullPolicy.alwaysPull()
-        )
-        container.start()
-        port = container.container.getFirstMappedPort
-      }) @@
-      TestAspect.after(ZIO.attempt(container.stop()))
+  EasyRacerClient.scenarios.zipWithIndex.foreach: (fn, number) =>
+    s"scenario ${number + 1}" should "work" in:
+      IOs.run(KyoApp.runFiber(fn(scenarioUrl)).toFuture).map(_.get).map(_ shouldBe "right")
