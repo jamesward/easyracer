19a3251533fc0835fb33182d691b9dba13e23316
James Ward <james@jamesward.com>
2024-05-22 02:40:20 -0600

    bump to kotlin 2.0.0 and cleanup tests

--- kotlin-arrow/src/test/kotlin/MainSpec.kt
+++ kotlin-arrow/src/test/kotlin/MainSpec.kt
@@ -2,29 +2,20 @@ import io.kotest.core.extensions.install
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.testcontainers.ContainerExtension
 import io.kotest.matchers.collections.shouldContainOnly
-//import org.slf4j.LoggerFactory
 import org.testcontainers.containers.GenericContainer
-//import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.images.PullPolicy
 
-class MainSpec : FunSpec() {
-    init {
-//        val logger = LoggerFactory.getLogger(this::class.java)
-//        val logConsumer = Slf4jLogConsumer(logger)
+class MainSpec : FunSpec({
+    val container = GenericContainer("ghcr.io/jamesward/easyracer")
+        .withExposedPorts(8080)
+        .withImagePullPolicy(PullPolicy.alwaysPull())
+        .waitingFor(Wait.forHttp("/"))
 
-        val container = GenericContainer("ghcr.io/jamesward/easyracer")
-            .withExposedPorts(8080)
-            .withImagePullPolicy(PullPolicy.alwaysPull())
-//            .withCommand("--debug")
-//            .withLogConsumer(logConsumer)
-            .waitingFor(Wait.forHttp("/"))
+    val server = install(ContainerExtension(container))
 
-        val server = install(ContainerExtension(container))
-
-        test("should work") {
-            fun url(i: Int) = "http://localhost:${server.firstMappedPort}/$i"
-            results(::url).shouldContainOnly("right")
-        }
+    test("should work") {
+        fun url(i: Int) = "http://localhost:${server.firstMappedPort}/$i"
+        results(::url).shouldContainOnly("right")
     }
-}
+})
--- kotlin-coroutines/build.gradle.kts
+++ kotlin-coroutines/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     application
-    kotlin("jvm") version "1.9.24"
+    kotlin("jvm") version "2.0.0"
 }
 
 repositories {
