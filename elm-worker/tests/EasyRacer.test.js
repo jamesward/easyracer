ca33699b31272bd4a2feb2c80a48ece890aa9505
Jack Leow <jackgene@gmail.com>
2024-06-30 15:34:31 -0700

    Log CPU load again...

--- elm-worker/tests/EasyRacer.test.js
+++ elm-worker/tests/EasyRacer.test.js
@@ -28,6 +28,7 @@ describe("EasyRacer", () => {
       if (typeof scenario.ports.sendCpuLoadRequest !== "undefined") {
         scenario.ports.sendCpuLoadRequest.subscribe(function () {
           os.cpuUsage(function (cpuLoad) {
+            console.log("CPU: " + cpuLoad * 100 + "%");
             scenario.ports.receiveCpuLoadResponse.send(cpuLoad);
           });
         });
