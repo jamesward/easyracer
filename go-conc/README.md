338b1ae493b20e7d71144fa485055c96f9cd136a
Jack Leow <jackgene@gmail.com>
2024-08-03 08:20:49 -0700

    Initial go-conc implementation.

---
+++ go-conc/README.md
@@ -0,0 +1,14 @@
+Pure Go
+-------
+
+Go implementation that uses only Go primitives and the standard library.
+
+Run the tests (using a Testcontainers scenario server):
+```
+go test ./...
+```
+
+Run against a manually started scenario server:
+```
+go run main.go
+```
---
+++ go-conc/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"testing"
+)
+
+func TestScenarios(t *testing.T) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "ghcr.io/jamesward/easyracer",
+		ExposedPorts: []string{"8080/tcp"},
+		WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
+	}
+
+	scenarioServer, containerErr := testcontainers.GenericContainer(
+		ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		},
+	)
+	if containerErr != nil {
+		t.Error(containerErr)
+	}
+	defer func() {
+		if err := scenarioServer.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err.Error())
+		}
+	}()
+
+	baseURL, endpointErr := scenarioServer.Endpoint(ctx, "http")
+	if endpointErr != nil {
+		t.Error(endpointErr)
+	}
+
+	scenarioURL := func(scenario int) string {
+		return fmt.Sprintf("%s/%d", baseURL, scenario)
+	}
+
+	for idx, scenario := range scenarios {
+		name := fmt.Sprintf("scenario%d", idx+1)
+		t.Run(name, func(t *testing.T) {
+			got := scenario(scenarioURL)
+			if got != "right" {
+				t.Errorf(`%s = "%s"; want "right"`, name, got)
+			}
+		})
+	}
+}
