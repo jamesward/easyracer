de0aee1bb342439eade85cfd0c1bea1de3c236da
James Ward <james@jamesward.com>
2025-01-27 13:59:16 -0700

    add server logging

--- java-cf/src/test/java/info/jab/easyracer/ScenariosTest.java
+++ java-cf/src/test/java/info/jab/easyracer/ScenariosTest.java
@@ -17,9 +17,13 @@ public class ScenariosTest {
     public void testScenarios() throws Exception {
         try (GenericContainer<?> scenarioServer = new GenericContainer<>(DockerImageName.parse(EASY_RACER_IMAGE))) {
             //Given
-            scenarioServer.withExposedPorts(8080).waitingFor(new HttpWaitStrategy()).withCommand("--debug").start()
+            scenarioServer.withExposedPorts(8080)
+                .waitingFor(new HttpWaitStrategy())
+                .withCommand("--debug")
+                .withLogConsumer(outputFrame -> System.out.print(outputFrame.getUtf8String()))
+                .start();
             var url = new URI("http://" + scenarioServer.getHost() + ":" + scenarioServer.getFirstMappedPort());
-            
+
             //When
             var scenarios = new Scenarios(url);
             var results = scenarios.results();
