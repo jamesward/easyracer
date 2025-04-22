bfcbaa659480c99ed82836cc6cfeb84463f3df66
James Ward <james@jamesward.com>
2025-04-22 00:37:48 -0600

    gids prep

--- .presos/engine.mjs
+++ .presos/engine.mjs
@@ -1,6 +1,7 @@
 import markdownItCodeSnippetEnhanced from '@gerhobbelt/markdown-it-code-snippet-enhanced'
-//import markdownMermaid from 'markdown-it-mermaid'
-//import markdownItKroki from '@kazumatu981/markdown-it-kroki'
+// It'd be nice if this worked, but it doesn't.
+// import markdownMermaid from 'markdown-it-mermaid'
+// So we use Kroki instead
+import markdownItKroki from '@kazumatu981/markdown-it-kroki'
 
-
-export default ({ marp }) => marp.use(markdownItCodeSnippetEnhanced) //.use(markdownMermaid)
+export default ({ marp }) => marp.use(markdownItCodeSnippetEnhanced).use(markdownItKroki) //.use(markdownMermaid)
