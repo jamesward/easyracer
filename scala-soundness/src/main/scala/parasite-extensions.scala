4d13854866c83a78ea42726b55298b570ea2cd36
Jack Leow <jackgene@gmail.com>
2025-05-19 12:01:46 -0700

    Cancel race losers more aggressively?

--- scala-soundness/src/main/scala/parasite-extensions.scala
+++ scala-soundness/src/main/scala/parasite-extensions.scala
@@ -4,5 +4,5 @@ import parasite.{AsyncError, Monitor, Task, race}
 extension [ResultType](tasks: Iterable[Task[ResultType]])
   def raceAndCancelLosers()(using Monitor, Codicil): ResultType raises AsyncError =
     val result = tasks.race()
-    tasks.each(task => if !task.ready then task.cancel())
+    tasks.each(task => task.cancel())
     result
