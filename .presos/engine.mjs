3a7ae454e92643ed1af901cf939bb35e2e5562ee
James Ward <james@jamesward.com>
2025-11-05 10:15:56 -0700

    marp mermaid workaround

--- .presos/engine.mjs
+++ .presos/engine.mjs
@@ -2,6 +2,6 @@ import markdownItCodeSnippetEnhanced from '@gerhobbelt/markdown-it-code-snippet-
 // It'd be nice if this worked, but it doesn't.
 // import markdownMermaid from 'markdown-it-mermaid'
 // So we use Kroki instead
-import markdownItKroki from '@kazumatu981/markdown-it-kroki'
+// import markdownItKroki from '@kazumatu981/markdown-it-kroki'
 
-export default ({ marp }) => marp.use(markdownItCodeSnippetEnhanced).use(markdownItKroki) //.use(markdownMermaid)
+export default ({ marp }) => marp.use(markdownItCodeSnippetEnhanced) //.use(markdownItKroki) //.use(markdownMermaid)
