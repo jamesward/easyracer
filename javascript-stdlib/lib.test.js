14d8521b6ac4801b9cda8f2d59a45ebf8fc40a19
James Ward <james@jamesward.com>
2025-12-29 15:09:55 -0700

    individual tests for each scenario

--- javascript-stdlib/lib.test.js
+++ javascript-stdlib/lib.test.js
@@ -15,16 +15,48 @@ describe("all work", () => {
         await container.stop()
     })
 
-    it("works", async () => {
+    it("scenario1 works", async () => {
         const httpPort = container.getFirstMappedPort()
         expect(await lib.scenario1(httpPort)).toBe("right")
+    }, 600000)
+
+    it("scenario2 works", async () => {
+        const httpPort = container.getFirstMappedPort()
         expect(await lib.scenario2(httpPort)).toBe("right")
+    }, 600000)
+
+    it("scenario3 works", async () => {
+        const httpPort = container.getFirstMappedPort()
         expect(await lib.scenario3(httpPort)).toBe("right")
+    }, 600000)
+
+    it("scenario4 works", async () => {
+        const httpPort = container.getFirstMappedPort()
         expect(await lib.scenario4(httpPort)).toBe("right")
+    }, 600000)
+
+    it("scenario5 works", async () => {
+        const httpPort = container.getFirstMappedPort()
         expect(await lib.scenario5(httpPort)).toBe("right")
+    }, 600000)
+
+    it("scenario6 works", async () => {
+        const httpPort = container.getFirstMappedPort()
         expect(await lib.scenario6(httpPort)).toBe("right")
+    }, 600000)
+
+    it("scenario7 works", async () => {
+        const httpPort = container.getFirstMappedPort()
         expect(await lib.scenario7(httpPort)).toBe("right")
+    }, 600000)
+
+    it("scenario8 works", async () => {
+        const httpPort = container.getFirstMappedPort()
         expect(await lib.scenario8(httpPort)).toBe("right")
+    }, 600000)
+
+    it("scenario9 works", async () => {
+        const httpPort = container.getFirstMappedPort()
         expect(await lib.scenario9(httpPort)).toBe("right")
     }, 600000)
 })
